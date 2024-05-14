42b464cfa1bf55feff3ad418e8baed00fcfd0099
Diego Martinez <diegomrey1@gmail.com>
2024-05-14 20:39:16 +0200

    add routes in signup and signin buttons

--- src/containers/NavBar.jsx
+++ src/containers/NavBar.jsx
@@ -1,9 +1,11 @@
 import Button from "@/components/Button";
 import { useAuth } from "@/contexts/Login";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 const NavBar = () => {
   const { user, verify, logout } = useAuth();
+  const router = useRouter();
 
   useEffect(() => {
     verify();
@@ -12,22 +14,21 @@ const NavBar = () => {
   return (
     <div className="bg-gray-900 h-16 flex justify-around items-center">
       <h1>Booking</h1>
-      {user && <h2>{user.name}</h2>}
       <div className="flex items-center gap-3">
         {!user ? (
           <>
-            <Button style={"light"} click={() => console.log("click")}>
+            <Button style={"light"} click={() => router.push("/login")}>
               Sign In
             </Button>
-            <Button style={"dark"} click={() => console.log("click")}>
+            <Button style={"dark"} click={() => router.push("/register")}>
               Sign Up
             </Button>
           </>
         ) : (
           <>
-            <h2>{user.name}</h2>{" "}
+            <h2>{user.name}</h2> <h2>{user.surname}</h2>
             <Button style={"dark"} click={logout}>
-              Sign Up
+              Logout
             </Button>
           </>
         )}
