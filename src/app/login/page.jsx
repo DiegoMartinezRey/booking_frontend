828a84a88cd1e97d5cee6a677ac005fc4c7ca1f8
Diego Martinez <diegomrey1@gmail.com>
2024-05-14 16:16:37 +0200

    add login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,7 +1,41 @@
+"use client";
+import Button from "@/components/Button";
+import InputField from "@/components/InputField";
+import { useState } from "react";
+
 const page = () => {
+  const [emailInput, setEmailInput] = useState("");
+  const [passwordInput, setPasswordInput] = useState("");
+
+  const getEmailInput = (text) => {
+    setEmailInput(text);
+  };
+  const getPasswordInput = (text) => {
+    setPasswordInput(text);
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="flex justify-center p-4">
+      <div className="flex flex-col items-center bg-slate-900 w-8/12 rounded-xl p-4">
+        <h1>Sign In</h1>
+        <InputField
+          label={"Email:"}
+          type={"string"}
+          value={emailInput}
+          placeholder={"example@email.com"}
+          setChange={getEmailInput}
+        />
+        <InputField
+          label={"Surname:"}
+          type={"string"}
+          value={passwordInput}
+          placeholder={"xxxxxxx"}
+          setChange={getPasswordInput}
+        />
+        <Button style={"dark"} link={"/"}>
+          Login
+        </Button>
+      </div>
     </div>
   );
 };
